059bb7e99265fc6d8cd75cd21db199184af3dc16
tp <antoine71.g@gmail.com>
2020-01-07 15:10:56 +0100

    debug

--- receive-captor.py
+++ receive-captor.py
@@ -3,6 +3,7 @@ import sys
 import serial
 import requests as rq
 import time, io, json
+import multiprocessing as mp
 
 # send serial message 
 # Don't forget to establish the right serial port ******** ATTENTION
@@ -10,43 +11,36 @@ import time, io, json
 SERIALPORT = "/dev/tty.usbserial-DA00G4XZ"
 BAUDRATE = 9600
 ser = serial.Serial()
-sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-last_timestamp=0
-def initUART():       
-    # ser = serial.Serial(SERIALPORT, BAUDRATE)
-    ser.port=SERIALPORT
-    ser.baudrate=BAUDRATE
-    ser.bytesize = serial.EIGHTBITS #number of bits per bytes
-    ser.parity = serial.PARITY_NONE #set parity check: no parity
-    ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-    ser.timeout = None          #block read
 
-    # ser.timeout = 0             #non-block read
-    # ser.timeout = 2              #timeout block read
-    ser.xonxoff = False     #disable software flow control
-    ser.rtscts = False     #disable hardware (RTS/CTS) flow control
-    ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
-    #ser.writeTimeout = 0     #timeout for write
-    
-    print ("Starting Up Serial Monitor")
-    try:
-            ser.open()
-    except serial.SerialException:
-            print("Serial {} port not available".format(SERIALPORT))
-            exit()
+last_timestamp=0
+
+        
+
 if len(sys.argv) > 1:
         SERIALPORT = "/dev/" + sys.argv[1]
 else:
         SERIALPORT = "/dev/ttyS5"
-initUART()
+ser=serial.Serial(SERIALPORT, 9600)
+        
 print("OK")
+
+def send_server(queue):
+        while True:
+                val = queue.get()
+                print(val)
+                headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+                r = rq.post("http://localhost:3001/fireInformation", val,headers=headers)
+                print(r.text)
+
+queue = mp.Queue()
+p = mp.Process(target=send_server,args=(queue,))
+p.start()
+
 while(True):
-        time.sleep(1)
+        #time.sleep(1)
         #sio.flush() # it is buffering. required to get the data out *now*
         text = ser.readline()
-        print(text)
         if(text):
-                headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-                r = rq.post("http://localhost:3001/fireInformation", text,headers=headers)
-                print(r.text)
+                queue.put(text)
+
 
